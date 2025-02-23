8a4b56dddb0e5b0350162350e6c69978e9fb2a06
Tanmay Tripathi <tanmaytrip59@gmail.com>
2025-02-23 23:41:15 +0530

    invalid message commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,12 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  std::string input;
 
   // Uncomment this block to pass the first stage
-   std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout<<input<<": command not found"<<std::endl;
+   while(true){
+ 	 std::cout << "$ ";
+ 	 std::getline(std::cin, input);
+  	 std::cout<<input<<": command not found"<<std::endl;
+	}
 }
