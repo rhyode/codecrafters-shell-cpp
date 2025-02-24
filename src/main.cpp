88f4a5c633de0b51415fbf63ad9de6ed9561bd57
Tanmay Tripathi <tanmaytrip59@gmail.com>
2025-02-24 15:38:20 +0530

    invalid message commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,34 @@
+
 #include <iostream>
 using namespace std;
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  std::string input;
+    // Flush after every std::cout / std:cerr
+    cout << unitbuf;
+    cerr << unitbuf;
+    string input;
 
-  // Uncomment this block to pass the first stage
-   while(true){
- 	 std::cout << "$ ";
- 	 std::getline(std::cin, input);
-	 if(input=="exit 0") break;
-	 string dec=input.substr(0,4);
-	 if(dec=="echo"){
-	 	if(input.length()>5){
-			cout<<input.substr(5)<<endl;
-		}
-	 }
-	elseif(dec=="type"){
-	   if(input.length()>5){
-		string type = input.substr(5);
-		if(type=="echo"||type=="exit"||type=="type") cout<<type<<" is a shell builtin"<<endl;
-		else cout<<input<<": not found"<<endl;
-	   }
-	else{
-  	 std::cout<<input<<": command not found"<<std::endl;
-	}	
-}
+    while(true) {
+        cout << "$ ";
+        getline(cin, input);
+        if(input=="exit 0") break;
+        
+        string dec=input.substr(0,4);
+        if(dec=="echo") {
+            if(input.length() > 5) {
+                cout << input.substr(5) << endl;
+            }
+        }
+        else if(dec=="type") {
+            if(input.length() > 5) {
+                string type = input.substr(5);
+                if(type=="echo"||type=="exit"||type=="type") 
+                    cout << type << " is a shell builtin" << endl;
+                else 
+                    cout << type  << ": not found" << endl;
+            }
+        }
+        else {
+            cout << input << ": command not found" << endl;
+        }
+    }
 }
