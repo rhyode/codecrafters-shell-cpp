21265b710af78ee313da289bca0ccec893d1a27b
Tanmay Tripathi <tanmaytrip59@gmail.com>
2025-03-05 18:35:02 +0530

    invalid message commit

--- src/main.cpp
+++ src/main.cpp
@@ -36,15 +36,7 @@ vector<string> parse_command(const string& input) {
             if(input[i] == '\\' && i + 1 < input.length()) {
                 if(!in_single_quotes) {
                     i++;
-                    if(input[i] == 'n') {
-                        if(in_double_quotes) {
-                            current_arg += "n";
-                        } else {
-                            current_arg += "\n";
-                        }
-                    } else {
-                        current_arg += input[i];
-                    }
+                    current_arg += input[i];
                     continue;
                 }
             }
